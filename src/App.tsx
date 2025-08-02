9d6860d4af303b842917e51656b9522f8d40eb25
Pramit Pal <pramitpal309@gmail.com>
2025-08-02 21:12:54 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className='font-mono text-center mt-2.5'>Configuration of React with Parcel, babel, and also jest </div>;
+};
+
+export default App;
